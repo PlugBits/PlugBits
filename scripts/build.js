b04e51b8ef1107afd90f6abcd84935a481725200
henpin-box <b.otkyaaa@gmail.com>
2025-09-03 21:54:02 -0400

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -226,7 +226,6 @@ const fs = require('fs');
 const path = require('path');
 const DIST = path.join(__dirname, '..', 'dist');
 
-// 既存の buildProducts()/buildIndexes()/copyStatic()/buildSitemap() の後に追加
 fs.writeFileSync(path.join(DIST, '.nojekyll'), '');
 console.log('Created .nojekyll');
 
